1fdfc799c07c639009bffb61516609654e7aff66
Ali <duboyal2@gmail.com>
2024-07-24 17:29:52 -0400

    ali change 2 edited main.py

--- project/backend/app/main.py
+++ project/backend/app/main.py
@@ -2,26 +2,41 @@
 
 # main.py
 
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+from flask import Flask
+
+app = Flask(__name__)
 
 
-class HelloWorldHandler(SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(b"Hello World")
-        else:
-            self.send_error(404, "File not found")
-
-
-def run(server_class=HTTPServer, handler_class=HelloWorldHandler):
-    server_address = ("", 8000)
-    httpd = server_class(server_address, handler_class)
-    print("Starting httpd server on port 8000...")
-    httpd.serve_forever()
+@app.route("/")
+def hello_world():
+    return "Hello World"
 
 
 if __name__ == "__main__":
-    run()
+    app.run(host="0.0.0.0", port=8000)
+
+
+# from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+# class HelloWorldHandler(SimpleHTTPRequestHandler):
+#     def do_GET(self):
+#         if self.path == "/":
+#             self.send_response(200)
+#             self.send_header("Content-type", "text/plain")
+#             self.end_headers()
+#             self.wfile.write(b"Hello World")
+#         else:
+#             self.send_error(404, "File not found")
+
+
+# def run(server_class=HTTPServer, handler_class=HelloWorldHandler):
+#     server_address = ("", 8000)
+#     httpd = server_class(server_address, handler_class)
+#     print("Starting httpd server on port 8000...")
+#     httpd.serve_forever()
+
+
+# if __name__ == "__main__":
+#     run()
