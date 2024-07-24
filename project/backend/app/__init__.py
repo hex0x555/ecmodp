eac96ed726cea95f5f791be77c8612c93ecd2cd1
Ali <duboyal2@gmail.com>
2024-07-24 17:18:36 -0400

    ali changes

---
+++ project/backend/app/__init__.py
@@ -0,0 +1,2 @@
+# might need this later when using flask
+# when not using http.server
--- project/backend/app/main.py
+++ project/backend/app/main.py
@@ -1,1 +1,27 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# main.py
+
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+class HelloWorldHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello World")
+        else:
+            self.send_error(404, "File not found")
+
+
+def run(server_class=HTTPServer, handler_class=HelloWorldHandler):
+    server_address = ("", 8000)
+    httpd = server_class(server_address, handler_class)
+    print("Starting httpd server on port 8000...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
