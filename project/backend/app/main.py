28460e2c944f77ac7c1547f7babeb8aa15c2588f
Ali <duboyal2@gmail.com>
2024-09-02 21:31:47 -0400

    ali adding basic arithmatic route

--- project/backend/app/main.py
+++ project/backend/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+
+@app.route("/add", methods=["GET"])
+def add_numbers():
+    # Get the values from the request arguments
+    a = request.args.get("num1", type=int)
+    b = request.args.get("num2", type=int)
+    # p = request.args.get("num2", type=int)
+
+    # Ensure both numbers are provided
+    if a is None or b is None:
+        return "Please provide both numbers as query parameters: ?num1=1&num2=2"
+
+    # Perform addition
+    result = a + b
+
+    # Return the result
+    return f"The result of {a} + {b} is {result}"
 
 
 if __name__ == "__main__":
